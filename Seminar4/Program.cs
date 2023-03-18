084840665abea623d5bf8362745a799ef88b1f7f
Valery1971 <podval1971@gmail.com>
2023-03-18 14:30:07 +0300

    Add Seminar4

---
+++ Seminar4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 24. Напишите программу, которая принимает число (А) и выдает сумму чисел от 1 до А.
+
+int Sum(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма от 1 до {number} = {Sum(number)}");
