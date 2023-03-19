9ca10b50e91bbb4ed1caf718d35d0806629bb77d
Valery1971 <podval1971@gmail.com>
2023-03-19 13:45:24 +0300

    Добавлена задача32

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,6 +1,39 @@
 ﻿// Задача 31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9,9]. 
 // Найдите сумму положительных и отрицательных элементов массива.
 
+// int[] FillArrayRandom(int size)
+// {
+//     int[] arr = new int[size];
+//     Random rnd = new Random(); // создаем объект класса (переменную типа Random)
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rnd.Next(-9, 10);
+//     }
+//     return arr;
+// }
+
+// Console.Write("Введите количество элементов массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] array = FillArrayRandom(size);
+
+// int sumPositive = 0;
+// int sumNegative = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if(array[i] > 0)
+//         sumPositive+= array[i];
+//     else
+//         sumNegative+= array[i];
+// }
+
+// Console.Write("Для массива: ");
+// Console.WriteLine($"[{string.Join(" ", array)}]");
+// Console.WriteLine($"Сумма положительных элементов равна {sumPositive}");
+// Console.WriteLine($"Сумма отрицательных элементов равна {sumNegative}");
+
+// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
 int[] FillArrayRandom(int size)
 {
     int[] arr = new int[size];
@@ -15,20 +48,11 @@ int[] FillArrayRandom(int size)
 Console.Write("Введите количество элементов массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] array = FillArrayRandom(size);
+int[] arrNeg = new int[size];
 
-int sumPositive = 0;
-int sumNegative = 0;
-
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < arrNeg.Length; i++)
 {
-    if(array[i] > 0)
-        sumPositive+= array[i];
-    else
-        sumNegative+= array[i];
+    arrNeg[i] = array[i]*-1;
 }
-
-Console.Write("Для массива: ");
-Console.WriteLine($"[{string.Join(" ", array)}]");
-Console.WriteLine($"Сумма положительных элементов равна {sumPositive}");
-Console.WriteLine($"Сумма отрицательных элементов равна {sumNegative}");
-
+Console.WriteLine($"Исходный массив: [{string.Join(" ", array)}]");
+Console.WriteLine($"Перевернутый массив: [{string.Join(" ", arrNeg)}]");
