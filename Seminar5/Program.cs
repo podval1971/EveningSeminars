2acff4fbb7f0b567f4c4788b61ffcd3bdf5c1629
Valery1971 <podval1971@gmail.com>
2023-03-19 14:11:25 +0300

    Добавлена задача33

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Задача 31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9,9]. 
 // Найдите сумму положительных и отрицательных элементов массива.
 
-// int[] FillArrayRandom(int size)
-// {
-//     int[] arr = new int[size];
-//     Random rnd = new Random(); // создаем объект класса (переменную типа Random)
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         arr[i] = rnd.Next(-9, 10);
-//     }
-//     return arr;
-// }
+int[] FillArrayRandom(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random(); // создаем объект класса (переменную типа Random)
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
 
-// Console.Write("Введите количество элементов массива: ");
-// int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
 // int[] array = FillArrayRandom(size);
 
 // int sumPositive = 0;
@@ -34,25 +34,41 @@
 
 // Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
 
-int[] FillArrayRandom(int size)
-{
-    int[] arr = new int[size];
-    Random rnd = new Random(); // создаем объект класса (переменную типа Random)
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = rnd.Next(-9, 10);
-    }
-    return arr;
-}
+// int[] array = FillArrayRandom(size);
+// int[] arrNeg = new int[size];
 
-Console.Write("Введите количество элементов массива: ");
-int size = Convert.ToInt32(Console.ReadLine());
+// for (int i = 0; i < arrNeg.Length; i++)
+// {
+//     arrNeg[i] = array[i]*-1;
+// }
+// Console.WriteLine($"Исходный массив: [{string.Join(" ", array)}]");
+// Console.WriteLine($"Перевернутый массив: [{string.Join(" ", arrNeg)}]");
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
 int[] array = FillArrayRandom(size);
-int[] arrNeg = new int[size];
-
-for (int i = 0; i < arrNeg.Length; i++)
-{
-    arrNeg[i] = array[i]*-1;
-}
 Console.WriteLine($"Исходный массив: [{string.Join(" ", array)}]");
-Console.WriteLine($"Перевернутый массив: [{string.Join(" ", arrNeg)}]");
+Console.Write("Введите искомое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if(array[i] == number)
+//     {
+//         Console.WriteLine($"Число {number} в массиве ЕСТЬ");
+//         return;
+//     }
+// }
+// Console.WriteLine($"Числа {number} в массиве НЕТ");
+
+//  другой вариант решения:
+string result = "НЕТ";
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] == number)
+    {
+        result = "ЕСТЬ";
+        break;
+    }
+}
+Console.WriteLine($"Число {number} в массиве {result}");
