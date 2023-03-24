948408e69aec2d344a3cad30e2b79c3d049a2578
Valery1971 <podval1971@gmail.com>
2023-03-24 23:40:10 +0300

    Добавлена задача 51

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -66,6 +66,55 @@
 // PrintMatrix(matrix);
 
 // Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// int[,] FillMatrixRandom(int row, int column)
+// {
+//     int[,] arr = new int[row,column];
+//     Random rnd = new Random(); // создаем объект класса (переменную типа Random)
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i,j] = rnd.Next(0, 10);
+//         }
+//     }
+//     return arr;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] PowEvenNumbers(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i+=2)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j+=2)
+//         {
+//             arr[i,j]*= arr[i,j];
+//         }
+//     }
+//     return arr;
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixRandom(row,column);
+// PrintMatrix(matrix);
+// PowEvenNumbers(matrix);
+// Console.WriteLine();
+// PrintMatrix(matrix);
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0), (1,1) и т.д)
 int[,] FillMatrixRandom(int row, int column)
 {
     int[,] arr = new int[row,column];
@@ -92,16 +141,20 @@ void PrintMatrix(int[,] array)
     }
 }
 
-int[,] PowEvenNumbers(int[,] arr)
+int SumMainDiagonal(int[,] arr)
 {
-    for (int i = 0; i < arr.GetLength(0); i+=2)
+    int sum = 0;
+    int index = 0;
+    if(arr.GetLength(0) <= arr.GetLength(1))
+        index = arr.GetLength(0);
+    else
+        index = arr.GetLength(1);
+
+    for (int i = 0; i < index; i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j+=2)
-        {
-            arr[i,j]*= arr[i,j];
-        }
+        sum = sum + arr[i,i];
     }
-    return arr;
+    return sum;
 }
 
 Console.Write("Введите количество строк массива: ");
@@ -110,6 +163,4 @@ Console.Write("Введите количество столбцов массив
 int column = Convert.ToInt32(Console.ReadLine());
 int[,] matrix = FillMatrixRandom(row,column);
 PrintMatrix(matrix);
-PowEvenNumbers(matrix);
-Console.WriteLine();
-PrintMatrix(matrix);
+Console.WriteLine($"Сумма диагональных элементов: {SumMainDiagonal(matrix)}");
