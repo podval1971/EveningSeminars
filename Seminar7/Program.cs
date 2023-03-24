33a8702ee52c7521ea00abc34100c7b4921a4b60
Valery1971 <podval1971@gmail.com>
2023-03-24 23:04:03 +0300

    Добавлена задача 49

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -33,14 +33,48 @@
 // PrintMatrix(matrix);
 
 // Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
-int[,] FillMatrixSum(int row, int column)
+// int[,] FillMatrixSum(int row, int column)
+// {
+//     int[,] arr = new int[row,column];
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i,j] = i + j;
+//         }
+//     }
+//     return arr;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixSum(row,column);
+// PrintMatrix(matrix);
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+int[,] FillMatrixRandom(int row, int column)
 {
     int[,] arr = new int[row,column];
+    Random rnd = new Random(); // создаем объект класса (переменную типа Random)
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            arr[i,j] = i + j;
+            arr[i,j] = rnd.Next(0, 10);
         }
     }
     return arr;
@@ -58,11 +92,24 @@ void PrintMatrix(int[,] array)
     }
 }
 
+int[,] PowEvenNumbers(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < arr.GetLength(1); j+=2)
+        {
+            arr[i,j]*= arr[i,j];
+        }
+    }
+    return arr;
+}
+
 Console.Write("Введите количество строк массива: ");
 int row = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов массива: ");
 int column = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = FillMatrixSum(row,column);
+int[,] matrix = FillMatrixRandom(row,column);
 PrintMatrix(matrix);
-
-// 
+PowEvenNumbers(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
