0f0e15922e2ffd97ebfca5072d555cee6ed2ba13
Valery1971 <podval1971@gmail.com>
2023-03-24 22:34:21 +0300

    Добавлена задача 46

---
+++ Seminar7/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+int[,] FillMatrixRandom(int row, int column)
+{
+    int[,] arr = new int[row,column];
+    Random rnd = new Random(); // создаем объект класса (переменную типа Random)
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = rnd.Next(0, 10);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+Console.Write("Введите количество строк массива: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixRandom(row,column);
+PrintMatrix(matrix);
