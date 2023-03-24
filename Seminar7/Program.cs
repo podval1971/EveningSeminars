8ca0c042e335a14bb0a8ab83205f365a4a2c1a27
Valery1971 <podval1971@gmail.com>
2023-03-24 22:48:10 +0300

    Добавлена задача 48

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -1,13 +1,46 @@
 ﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
-int[,] FillMatrixRandom(int row, int column)
+// int[,] FillMatrixRandom(int row, int column)
+// {
+//     int[,] arr = new int[row,column];
+//     Random rnd = new Random(); // создаем объект класса (переменную типа Random)
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i,j] = rnd.Next(0, 10);
+//         }
+//     }
+//     return arr;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixRandom(row,column);
+// PrintMatrix(matrix);
+
+// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+int[,] FillMatrixSum(int row, int column)
 {
     int[,] arr = new int[row,column];
-    Random rnd = new Random(); // создаем объект класса (переменную типа Random)
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            arr[i,j] = rnd.Next(0, 10);
+            arr[i,j] = i + j;
         }
     }
     return arr;
@@ -25,11 +58,11 @@ void PrintMatrix(int[,] array)
     }
 }
 
-
-
 Console.Write("Введите количество строк массива: ");
 int row = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов массива: ");
 int column = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = FillMatrixRandom(row,column);
+int[,] matrix = FillMatrixSum(row,column);
 PrintMatrix(matrix);
+
+// 
